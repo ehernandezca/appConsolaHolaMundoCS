75d153577bcf305999f2fed6c549f694712836d2
Efrain <Efrain@EFRAIN>
2021-08-29 12:57:52 -0500

    Add project files.

---
+++ appConsolaHolaMundoCS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace appConsolaHolaMundoCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            //Sumar
+            int a = 3;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine(a + "+" + b + "=" + c);
+            //Multiplicar 
+            int d = a * b;
+            Console.WriteLine(a + "*" + b+ "=" + d);
+            //Restar 
+            int e = b - a;
+            Console.WriteLine(b + "-" + a + "=" + e);
+            Console.ReadKey();
+        }
+    }
+}
